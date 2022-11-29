24eab63e67888e45b68a5d976b89122f6c9b1613
Paper <paper@volleythat.com>
2022-11-28 21:14:12 -0800

    Make the voice sample

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private void SetColor(Transform trans, Color color)
+    {
+        trans.GetComponent<Renderer>().material.color = color;
+    }
+
+    public void UpdateColor(string[] values)
+    {
+        var colorString = values[0];
+        var shapeString = values[1];
+
+        if (!ColorUtility.TryParseHtmlString(colorString, out var color)) return;
+        if (string.IsNullOrEmpty(shapeString)) return;
+
+        foreach (Transform child in transform)
+        {
+            if (child.name.IndexOf(shapeString, StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                SetColor(child, color);
+                return;
+            }
+        }
+    }
+}
