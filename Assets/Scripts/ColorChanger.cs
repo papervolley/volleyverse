857d8090a34a89c6aec7603dc3ff8b3f0d8c0be5
Paper <paper@volleythat.com>
2022-11-28 22:07:12 -0800

    Make the voice sample work

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -10,6 +10,7 @@ public class ColorChanger : MonoBehaviour
 
     public void UpdateColor(string[] values)
     {
+        Debug.Log("Update!" + values[1]);
         var colorString = values[0];
         var shapeString = values[1];
 
@@ -20,6 +21,7 @@ public class ColorChanger : MonoBehaviour
         {
             if (child.name.IndexOf(shapeString, StringComparison.OrdinalIgnoreCase) != -1)
             {
+                Debug.Log("match!");
                 SetColor(child, color);
                 return;
             }
