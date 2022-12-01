90e54a1db52e503deebbae9cce54895f68ab118f
Paper <paper@volleythat.com>
2022-12-01 13:55:23 -0800

    Look at the bag to activate voice

---
+++ Assets/RayManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RayManager : MonoBehaviour
+{
+    public enum MicIntention
+    {
+        Activate,
+        Deactivate
+    }
+
+    private MicIntention micIntention = MicIntention.Deactivate;
+
+    [SerializeField]
+    private Camera Eye;
+
+    [SerializeField]
+    private TextMeshPro Debugger;
+
+    [SerializeField]
+    private Oculus.Voice.AppVoiceExperience VoiceExperience = null;
+
+    [SerializeField]
+    private Renderer bag = null;
+
+    [SerializeField] private LayerMask layermask;
+
+    private void FixedUpdate() {
+        Vector3 fwd = Eye.transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(Eye.transform.position, fwd, out hit, 10.0f, layermask))
+        {
+            GameObject hitObj = hit.transform.gameObject;
+            Debug.Log("hitting " + hitObj.tag);
+            Debugger.text = hitObj.name;
+            // activate voice
+            if (micIntention != MicIntention.Activate)
+            {
+                micIntention = MicIntention.Activate;
+                VoiceExperience.Activate();
+            }
+        } else {
+            Debugger.text = "not hitting anything";
+        }
+    }
+
+    public void OnVoiceActivate()
+    {
+        bag.material.color = Color.yellow;
+    }
+
+    public void OnVoiceDeactive()
+    {
+        micIntention = MicIntention.Deactivate;
+        bag.material.color = Color.white;
+    }
+}
